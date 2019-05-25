fc73b067a78486de6873f5d2634d72ed2c799d74
Waga <wagaodongo@gmail.com>
2019-05-25 21:55:46 +0300

    static files

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index( request):
-    return render('<h1> Hello World</h1>')
+    return render(request, 'pages/index.html')
 
 def about (request):
-    return 
+    return render(request, 'pages/about.html')
