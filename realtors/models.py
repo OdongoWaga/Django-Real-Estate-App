2a990fbaf9d8b04fa4c2a52534cae7c9402c4239
Waga <wagaodongo@gmail.com>
2019-05-26 21:21:33 +0300

    migrations

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from datetime import datetime
+
+# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_mvp= models.BooleanField(default=True)
+    hire_date_date= models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.name
