1d074c10c03b39fa07e9376ada4e70fbc3a38da6
Waga <wagaodongo@gmail.com>
2019-05-28 22:59:37 +0300

    changes

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.choices import price_choices, bedroom_choices, state_choices
+
 from listings.models import Listing
 from realtors.models import Realtor
-
-# Create your views here.
 
 
 def index(request):
     listings = Listing.objects.order_by(
         '-list_date').filter(is_published=True)[:3]
+
     context = {
         'listings': listings,
         'state_choices': state_choices,
@@ -21,7 +21,7 @@ def index(request):
 
 
 def about(request):
-    # get All realtors
+    # Get all realtors
     realtors = Realtor.objects.order_by('-hire_date')
 
     # Get MVP
